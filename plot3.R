1159baf77196cebd45349f58132a79b54b30154b
Christian L. Schultz <clschultz_at_gmail.com>
2014-07-09 13:00:18 -0700

    plot3 skeleton code completed

---
+++ plot3.R
@@ -0,0 +1,21 @@
+plot3 <- function(x) {
+    plot(data$datetime, data$Sub_metering_1, type="n", 
+         xlab="", ylab="Energy sub metering")
+    lines(data$datetime, data$Sub_metering_1, col="black")
+    lines(data$datetime, data$Sub_metering_2, col="red")
+    lines(data$datetime, data$Sub_metering_3, col="blue")
+    legend("topright", 
+           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+           lty=1, lwd=1, col=c("black", "red", "blue"))
+
+    png(file="plot3.png", width=480, height=480, bg="transparent")
+    plot(data$datetime, data$Sub_metering_1, type="n", 
+         xlab="", ylab="Energy sub metering")
+    lines(data$datetime, data$Sub_metering_1, col="black")
+    lines(data$datetime, data$Sub_metering_2, col="red")
+    lines(data$datetime, data$Sub_metering_3, col="blue")
+    legend("topright", 
+           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+           lty=1, lwd=1, col=c("black", "red", "blue"))
+    dev.off()
+}
