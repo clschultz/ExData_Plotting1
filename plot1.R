065d3178a424b4faa64b4d91d827c9aa867c2f17
Christian L. Schultz <clschultz_at_gmail.com>
2014-07-09 11:32:12 -0700

    plot1 code completed
    successfully creates plot1 as required as well as png file...still to
    integrate with the subsetting code

---
+++ plot1.R
@@ -0,0 +1,9 @@
+plot1 <- function(x) {
+    hist(x$Global_active_power, col="red", main="Global Active Power",
+         xlab="Global Active Power (kilowatts)")
+    
+    png(file="plot1.png", width=480, height=480)
+    hist(x$Global_active_power, col="red", main="Global Active Power",
+         xlab="Global Active Power (kilowatts)")
+    dev.off()
+}
