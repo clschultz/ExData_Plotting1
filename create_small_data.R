906b1953b9198acdb3405baf02329951e2f92552
Christian L. Schultz <clschultz_at_gmail.com>
2014-07-09 11:31:29 -0700

    base code for subsetting data for required dates
    Code skeleton for integrating into individual files later

--- create_small_data.R
+++ create_small_data.R
@@ -3,5 +3,13 @@ create_small_data <- function() {
     data <- read.csv("household_power_consumption.txt", 
                      na.strings="?", 
                      sep=";",
-                     nrows=6843)
+#                      nrows=30000,
+                     colClasses=c("character", "character", "numeric", "numeric",
+                                  "numeric", "numeric", "numeric", "numeric",
+                                  "numeric"))
+    
+    # convert Date column to Date type
+    data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+    data <- subset(data, (Date == "2007-02-01" | Date == "2007-02-02"))
+    print(data)
 }
