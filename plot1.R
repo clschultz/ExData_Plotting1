d7471c27fc917f35cfe2f0390411d1678e51b473
Christian L. Schultz <clschultz_at_gmail.com>
2014-07-09 13:00:35 -0700

    added transparent backgrounds

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@ plot1 <- function(x) {
     hist(x$Global_active_power, col="red", main="Global Active Power",
          xlab="Global Active Power (kilowatts)")
     
-    png(file="plot1.png", width=480, height=480)
+    png(file="plot1.png", width=480, height=480, bg="transparent")
     hist(x$Global_active_power, col="red", main="Global Active Power",
          xlab="Global Active Power (kilowatts)")
     dev.off()
--- plot2.R
+++ plot2.R
@@ -2,7 +2,7 @@ plot2 <- function(x) {
     plot(data$datetime, data$Global_active_power, type="l", 
          ylab="Global Active Power (kilowatts)", xlab="")
     
-    png(file="plot2.png", width=480, height=480)
+    png(file="plot2.png", width=480, height=480, bg="transparent")
     plot(data$datetime, data$Global_active_power, type="l", 
          ylab="Global Active Power (kilowatts)", xlab="")
     dev.off()
